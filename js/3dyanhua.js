2e78777d1c7e8e9952edfdd6e52332462215126b
denine <deninelu@163.com>
2020-02-11 23:50:41 +0800

    test

--- js/3dyanhua.js
+++ js/3dyanhua.js
@@ -287,3 +287,10 @@ frame();
 setTimeout(function() {
 	document.getElementById("btnshow").style.display="block";
 }, 3000);
+
+var button=new button();
+button.label="CLICK";
+a2.addChild(button);
+a2.x=12;
+a2.y=13;
+
